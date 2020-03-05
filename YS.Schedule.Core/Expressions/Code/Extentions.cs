456e92d61e7598de1bfbdc317dfefea393460657
ypb <obpt@sina.com>
2020-03-06 01:32:47 +0800

    add translate expression

---
+++ YS.Schedule.Core/Expressions/Code/Extentions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Data;
+using System.Collections.Generic;
+namespace YS.Schedule.Expressions.Code
+{
+    internal static class Extentions
+    {
+        static Dictionary<string, string> ReplaceDic = new Dictionary<string, string>()
+        {
+            ["!="] = "<>",
+            ["&&"] = "And",
+            ["&"] = "And",
+            ["||"] = "Or",
+            ["|"] = "Or"
+        };
+        public static string ToDataTableExpression(this string originExpression)
+        {
+            return originExpression.Replace(ReplaceDic);
+        }
+    }
+}
