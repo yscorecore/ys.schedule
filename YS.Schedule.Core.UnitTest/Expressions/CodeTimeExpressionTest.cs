5e06d3538c2417b74d321b02382135f198d0eafa
ypb <obpt@sina.com>
2020-03-07 12:52:21 +0800

    add exception logic

--- YS.Schedule.Core.UnitTest/Expressions/CodeTimeExpressionTest.cs
+++ YS.Schedule.Core.UnitTest/Expressions/CodeTimeExpressionTest.cs
@@ -9,7 +9,7 @@ namespace YS.Schedule.Expressions
     public class CodeTimeExpressionTest
     {
         [DataRow("", "2020-03-02", true)]
-        [DataRow("     ", "2020-03-02", true)]
+        [DataRow("     \t ", "2020-03-02", true)]
         [DataRow("y=2020", "2020-03-02", true)]
         [DataRow("y=2020", "2019-03-02", false)]
         [DataRow("y!=2020", "2020-03-02", false)]
@@ -23,7 +23,8 @@ namespace YS.Schedule.Expressions
         [DataRow("y/2=1010", "2020-03-02", true)]
         [DataRow("y%100=20", "2020-03-02", true)]
         [DataRow("y=2020&M=3&d=2", "2020-03-02", true)]
-
+        [DataRow("y+(M+d)*2=2030", "2020-03-02", true)]
+        [DataRow("M=3&d=2&h=8&m=40&s=23", "2020-03-02 08:40:23", true)] 
         [DataTestMethod]
         public void ShouldReturnExpectedValueWhenMatch(string expression, string dateTimeText, bool expected)
         {
@@ -42,30 +43,33 @@ namespace YS.Schedule.Expressions
             }
         }
 
-        [DataRow(null, typeof(ArgumentNullException), null)]
-        [DataRow("Abc>5", typeof(CodeTimeExpressionException), "Unknow time part 'Abc'.")]
-
+        //[DataRow(null, typeof(ArgumentNullException), null)]
+        [DataRow("Abc>5", "Unknow time part 'Abc'.")]
+        [DataRow("[y>2015]", "Illegal character at position 1.")]
+        [DataRow("(y>2015))", null)]
+        [DataRow("y*2", "The expression should return a bool value.")]
         [DataTestMethod]
-        public void ShouldThrowExceptionWhenNewCodeTimeExpression(string expression, Type exceptionType, string exceptionMessage)
+        public void ShouldThrowExceptionWhenNewCodeTimeExpression(string expression, string exceptionMessage)
         {
-            try
+            var exception = Assert.ThrowsException<CodeTimeExpressionException>(() =>
             {
                 using (new CodeTimeExpression(expression))
                 {
                 }
-            }
-#pragma warning disable CA1031 // 不捕获常规异常类型
-            catch (Exception ex)
-#pragma warning restore CA1031 // 不捕获常规异常类型
+            });
+            if (exceptionMessage != null)
             {
-                Assert.AreEqual(exceptionType, ex.GetType());
-                if (exceptionMessage != null)
-                {
-                    Assert.AreEqual(exceptionMessage, ex.Message);
-                }
+                Assert.AreEqual(exceptionMessage, exception.Message);
             }
+        }
 
-
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ShouldThrowArgumentNullExceptionWhenNewCodeTimeExpressionGiveExpressionIsNull()
+        {
+            using (new CodeTimeExpression(null))
+            {
+            }
         }
     }
 }
